7ec963df6c247aa5d2d0cf6609f0781a0c689684
Sashok121 <kindras1@rambler.ru>
2014-10-03 19:00:08 +0300

    add php file

--- js/script.js
+++ js/script.js
@@ -1,48 +1,115 @@
-var slider = $('#slider article.block');
-var index = 0;
-setInterval(function(){
-	var lastIndex = index;
-	if(index+1 < slider.length){
-		index++
-	} else {
-		index = 0;
-	}
-	$(slider[index]).css({"visibility": "visible","right":"-102%","z-index":1})
-	$(slider[lastIndex]).css("z-index","0").animate({right:"102%",visibility:"hidden"},1000);
-		$(slider[index]).animate({right:"0"},1000);	
+jQuery(function($){
+	
+	var slider = $('#slider article.block');
+	var index = 0;
+	setInterval(function(){
+		var lastIndex = index;
+		if(index+1 < slider.length){
+			index++
+		} else {
+			index = 0;
+		}
+		$(slider[index]).css({"visibility": "visible","right":"-102%","z-index":1})
+		$(slider[lastIndex]).css("z-index","0").animate({right:"102%",visibility:"hidden"},1000);
+			$(slider[index]).animate({right:"0"},1000);	
 
-},5000);
+	},5000);
+    $("a.btn-send").click(function(){
+        $("#modal").show();
+    });
+    $("input[name = phone]").mask("+380(99) 999-99-99");
+    $('#modal').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength:5,
+                maxlength:100
+            },
+            email: {
+                required: true,
+                maxlength:100,
+                email: true
+            },
+            phone: {
+                required: false,
+                maxlength:20
+            },
+           comment: {
+               required: false,
+                maxlength:1000
+            }
 
-$('a.btn-send').click(function(){
-	$('#modal').fadeToggle('slow');
-});
+        },
+        message:{
+            name: {
+                required: "Заполните ФИО",
+                minlength:"Min букв 5",
+                maxlength:"Мах букв 100"
+            },
+            email: {
+                required: "Введите адрес",
+                maxlength:"Мах букв 100",
+                email: "Введите правильный адрес"
+            },
+            phone: {
 
-$('#modal a.close').click(function close(){
-	$.each($('#modal input,#modal textarea'),function(i,value){
-		value.value = "";
-	});
-		
+                maxlength:"Мах 20 цыфр"
 
 
-
-
-
-	$('#modal').fadeToggle('slow');
-
-});
-
-function cform(){
-    var str = "ajax=1";
-    $.ajax({
-        url: "/contact/",
-        type: "post",
-        data: str,
-        cache: false,
-        success: function(response){
-            $.fancybox(response);
-
-            return true;
-
+            },
+            comment: {
+                maxlength:"Мах букв 1000"
+            }
+        },
+        success: function(label) {
+            label.text('OK!').addClass('valid');
         }
-    })
-}
+    });
+
+
+
+
+$("#modal button[type=submit]").click(function(){
+
+    $.post(
+        "send.php",
+        {
+            name: $("input[name=name]").val(),
+            email: $("input[name=email]").val(),
+            phone: $("input[name=phone]").val(),
+            comment: $("textarea[name=comment]").val()
+
+
+        },
+        onAjaxSuccess
+    );
+
+    function onAjaxSuccess(data)
+    {
+        // Здесь мы получаем данные, отправленные сервером и выводим их на экран.
+        alert(data);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
---
+++ send.php
@@ -0,0 +1,4 @@
+<?php
+  // файл http://hostname/ajaxtest.php
+  echo "I get param1 = ".$_POST['name']." and param2 = ".$_POST['email']." and phone = ".$_POST['phone']." and message = ".$_POST['comment'];
+?>
